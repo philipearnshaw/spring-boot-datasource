839367cf3226f5b8faf0459a6af9287947f7db97
philipearnshaw <philip.earnshaw@sky.uk>
2018-01-22 13:36:51 +0000

    Base 2.0.0.M7 project

---
+++ src/main/java/springbootdatasource/SpringBootDatasourceApplication.java
@@ -0,0 +1,12 @@
+package springbootdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDatasourceApplication.class, args);
+	}
+}
