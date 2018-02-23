2cb4f334dc9bc567f22efb3742cf7c4d518eb8ff
philipearnshaw <philip.earnshaw@sky.uk>
2018-02-23 12:02:52 +0000

    Expanded repository IT tests

--- src/test/java/springbootdatasource/repositories/CompetitionRepositoryIT.java
+++ src/test/java/springbootdatasource/repositories/CompetitionRepositoryIT.java
@@ -1,6 +1,8 @@
 package springbootdatasource.repositories;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
 
@@ -17,16 +19,29 @@ import springbootdatasource.model.Competition;
 @SpringBootTest
 public class CompetitionRepositoryIT {
 
+    private static final String COMPETITION_NAME = "name";
+    
     @Autowired
     CompetitionRepository competitionRepository;
     
     @Before
     public void setUp() throws Exception {
+        final Competition competition = new Competition(COMPETITION_NAME, "shortName", "shortCode");
+        competitionRepository.save(competition);
     }
 
     @Test
-    public void testFindByName() {
-        Optional<Competition> competition = competitionRepository.findByName("competition-name-1");
-        assertEquals("competition-name-1", competition.get().getName());
+    public void testFindByName_ShouldNotReturnCompetition() {
+        final Optional<Competition> optionalCompetition = competitionRepository.findByName("unknown-name");
+        
+        assertFalse("Competition is present", optionalCompetition.isPresent());
+    }
+    
+    @Test
+    public void testFindByName_ShouldReturnCompetition() {
+        final Optional<Competition> optionalCompetition = competitionRepository.findByName(COMPETITION_NAME);
+        
+        assertTrue("Competition is not present", optionalCompetition.isPresent());
+        assertEquals("Competition should be called " + COMPETITION_NAME, COMPETITION_NAME, optionalCompetition.get().getName());
     }
 }
